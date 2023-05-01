a0359a928bd998d9c4708dec71e6fc8d9f661d07
Rodrigo F Maceno <rferreira.laba@solvd.com>
2023-05-01 02:15:22 -0600

    updated

--- army/src/main/java/Main.java
+++ army/src/main/java/Main.java
@@ -1,18 +1,18 @@
 // Soldier, Sailor, Airman have overridden methods
-// Weapon, Person, Vehicle are all abstract 
+// Weapon, Person, Vehicle are all abstract
 public class Main {
     public static void main(String[] args) {
 
         Army army1 = new Army();
-        army1.createArmy("US");
-        army1.createNavy("US");
-        army1.createAirForce("US");
+        army1.createArmy("Country 1");
+        army1.createNavy("Country 1");
+        army1.createAirForce("Country 1");
 
 
         Army army2 = new Army();
-        army2.createArmy("Brazil");
-        army2.createNavy("Brazil");
-        army2.createAirForce("Brazil");
+        army2.createArmy("Country 2");
+        army2.createNavy("Country 2");
+        army2.createAirForce("Country 2");
 
         //test combat functionality
         Airman.combat(army1.getAirmen()[0], army2.getAirmen()[0], new Revolver(), new Revolver());

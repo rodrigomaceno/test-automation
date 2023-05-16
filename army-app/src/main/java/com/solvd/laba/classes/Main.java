a4e7442bcc62aa07062778a3400ba219c06bf493
Rodrigo Maceno <maceno@gmx.com>
2023-05-16 07:56:25 -0600

    .

--- army-app/src/main/java/com/solvd/laba/classes/Main.java
+++ army-app/src/main/java/com/solvd/laba/classes/Main.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 // Printable interface has a static method
 // Airman, Soldier and Marine have final and static fields and overridden methods
 // Weapon has a final property damageToTarget
-// Army has a static combat() and whoWon() methods
+// Army has static combat() and whoWon() methods
 // Army has collections
 // Person and Vehicle are abstract
 public class Main {

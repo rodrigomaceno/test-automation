080bc62c541aa1ecdc95d83d289a17dcb6b0b34a
Rodrigo Maceno <maceno@gmx.com>
2023-05-21 03:42:42 -0600

    file writer

--- army-app/src/main/java/com/solvd/laba/classes/file/reader/FileReaderApp.java
+++ army-app/src/main/java/com/solvd/laba/classes/file/reader/FileReaderApp.java
@@ -12,35 +12,34 @@ public class FileReaderApp {
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/myfile.txt");
 
-        FileReader a = new FileReader(file);
-
-        BufferedReader b = new BufferedReader(a);
-
         HashSet<String> list = new HashSet<>();
 
+        try (BufferedReader b = new BufferedReader(new FileReader(file))) {
 
-        String s;
-        String word = "";
+            String s;
 
-        while ((s = b.readLine()) != null) {
-            for (char c : s.toCharArray()) {
-                if (c != ' ')
-                    word += c;
-                else {
-                    if (!word.equals("")) {
-                        list.add(word);
-                        word = "";
+            String word = "";
+
+            while ((s = b.readLine()) != null) {
+                for (char c : s.toCharArray()) {
+                    if (c != ' ')
+                        word += c;
+                    else {
+                        if (!word.equals("")) {
+                            list.add(word);
+                            word = "";
+                        }
                     }
                 }
             }
         }
 
-        a.close();
-        b.close();
+
         logger.info("Number of unique words: " + list.size());
-        FileWriter writer = new FileWriter("src/main/resources/myfile.txt", true);
-        writer.write((String.valueOf(list.size())));
-        writer.close();
+
+        try (FileWriter writer = new FileWriter("src/main/resources/myfile.txt", true)) {
+            writer.write((String.valueOf(list.size())));
+        }
 
     }
 }
